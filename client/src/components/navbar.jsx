b366a2c8a3e1855768c0eb6453cc1ac2020167d6
ymnwri <tiuprof77@gmail.com>
2025-07-26 10:20:57 +0800

    chore: add suggestions for routing

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Navbar = () => {
   return (
     <header className="flex flex-col items-center p-6 shadow-md">
       <img src="/logo.png" alt="Accentex Logo" className="h-12 mb-4" />
       <nav className="flex gap-6 text-lg font-medium">
+        {/* Palitan to ng <Link> components for better routing */}
         <a href="#" className="hover:underline">Home</a>
         <a href="#" className="hover:underline">About Us</a>
         <a href="#" className="hover:underline">Services</a>
