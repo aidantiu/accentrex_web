8cb9b0bfa7e8b2874c61b98ff07468edb01198f6
John Laurence G. Rejoso <johnlaurencegrejoso@iskolarngbayan.pup.edu.ph>
2025-08-01 14:53:40 +0800

    feat: add services page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/homepage.jsx'
 import Health from './pages/health.jsx';
 import AboutUs from './pages/aboutuspage.jsx';
+import Services from './pages/servicespages.jsx';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/about" element={<AboutUs />} />
         <Route path="/" element={<Homepage />} />
         <Route path="/health" element={<Health />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </BrowserRouter>
   );
